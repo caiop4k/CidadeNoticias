cf2058c5d6966ff2914f7187f880df974c9562db
Caio Alves Vitor <72303016+caiop4k@users.noreply.github.com>
2022-04-28 15:39:50 -0300

    Add files via upload

---
+++ javascript/cab.js
@@ -0,0 +1,28 @@
+const cab = document.getElementById("cabecalho01").innerHTML = `<div id="container">
+            <div id="topo">
+                <h1 class="logo">Noticias Cidade</h1>
+                <ul id="navegacao">
+                    <li class="primeiro">
+                        <a id="home" href="index.html">Home</a>
+                    </li>
+                    <li>
+                        <a id="brasil" href="brasil.html">Brasil</a>
+                    </li>
+                    <li>
+                        <a id="internacional" href="internacional.html">Internacional</a>
+                    </li>
+                    <li>
+                        <a id="economia" href="economia.html">Economia</a>
+                    </li>
+                    <li>
+                        <a id="saude" href="saude.html">Saude</a>
+                    </li>
+                    <li>
+                        <a id="cienia" href="ciencia.html">Ciência</a>
+                    </li>
+                    <li>
+                        <a id="fotos" href="fotos.html">Fotos</a>
+                    </li>
+                </ul>
+            </div>
+        </div>`;
